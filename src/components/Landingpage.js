f53cbddfbbb3346f71383cd89875ccddd4b9c779
bunmigrey <bunmigrey@icloud.com>
2022-12-22 19:39:56 +0100

    adding footer section

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -45,9 +45,10 @@ const Landingpage = () => {
             <div className='bodyDiv'>
                 <div className='bodyDivSub'>
                     <div className='bodyDiv_h1Text'><h1>Ethereum Blockchain Explorer</h1></div>
-                    <div className='bodyDiv_paraText'><h3>Get access to all transactions that occur on the ethereum blockchain.</h3>
-                    </div>
-                    <div className='bodyDiv_input' ><div className='ethlogoSearch'><i className="fa-brands fa-ethereum"></i><span>ETH</span></div>
+                    <div className='bodyDiv_paraText'><h3>Get access to all transactions that occur on the ethereum blockchain.</h3></div>
+                    <div className='bodyDiv_input' >
+                        <div className='ethlogoSearch'><i className="fa-brands fa-ethereum fa-ethereumlogo"></i><span>ETH</span>
+                        </div>
                         <i class="fa-solid fa-magnifying-glass"></i><input placeholder="Search by Address, Transaction, or Block"></input></div>
                     <div className='bodyDiv_inputText'>
                         <div><i class="fa-solid fa-wallet"></i><span className='bodyDiv_searchText' id='bodyDiv_searchTextAdd'>Address</span></div>
@@ -58,96 +59,152 @@ const Landingpage = () => {
             </div>
 
             <div className='infoDis'>
-            <div className='infoDisplayDiv'>
-                <div className='infoDisplayFirstDiv'>
-                    <i className="fa-brands fa-ethereum"></i><div className='infoDisplayPriceDiv'><span>ETHER PRICE</span></div>
-                    <div className='infoDisplayValueDiv'>
-                        <div className='infoDisplayValueDiv1'>
-                            <span>$ 1, 182.32</span>
-                        </div>
-                        <div className='infoDisplayValueDiv2'>
-                            <span>2.58</span><i class="fa-solid fa-angle-down"></i>
-                        </div>
+                <div className='infoDisplayDiv'>
+                    <div className='infoDisplayFirstDiv'>
+                        <i className="fa-brands fa-ethereum"></i><div className='infoDisplayPriceDiv'><span>ETHER PRICE</span></div>
+                        <div className='infoDisplayValueDiv'>
+                            <div className='infoDisplayValueDiv1'>
+                                <span>$ 1, 182.32</span>
+                            </div>
+                            <div className='infoDisplayValueDiv2'>
+                                <span>2.58</span><i class="fa-solid fa-angle-down"></i>
+                            </div>
 
+                        </div>
+                        <div className='vh'></div>
+                        <div className='vl'></div>
                     </div>
-                    <div className='vh'></div>
-                    <div className='vl'></div>
-                </div>
 
-                <div className='infoDisplayFirstDiv'>
-                    <i class="fa-solid fa-arrow-right-arrow-left"></i><div className='infoDisplayPriceDiv'><span>TRANSACTIONS</span></div>
-                    <div className='infoDisplayValueDiv'>
-                        <div className='infoDisplayValueDiv1'>
-                            <span>$ 1, 182.32</span>
+                    <div className='infoDisplayFirstDiv'>
+                        <i class="fa-solid fa-arrow-right-arrow-left"></i><div className='infoDisplayPriceDiv'><span>TRANSACTIONS</span></div>
+                        <div className='infoDisplayValueDiv'>
+                            <div className='infoDisplayValueDiv1'>
+                                <span>$ 1, 182.32</span>
+                            </div>
+                            <div className='infoDisplayValueDiv2'>
+                                <span>2.58</span><i class="fa-solid fa-angle-down"></i>
+                            </div>
+                            <div className='vh2'></div>
+                            <div className='vl2'></div>
                         </div>
-                        <div className='infoDisplayValueDiv2'>
-                            <span>2.58</span><i class="fa-solid fa-angle-down"></i>
-                        </div>
-                        <div className='vh2'></div>
-                        <div className='vl2'></div>
                     </div>
-                </div>
 
-                <div className='infoDisplayFirstDiv infoDisplayFirstDivBlock'>
-                    <i class="fa-solid fa-cube"></i><div className='infoDisplayPriceDiv'><span>BLOCKS</span></div>
-                    <div className='infoDisplayValueDiv'>
-                        <div className='infoDisplayValueDiv1'>
-                            <span>$ 1, 182.32</span>
+                    <div className='infoDisplayFirstDiv infoDisplayFirstDivBlock'>
+                        <i class="fa-solid fa-cube"></i><div className='infoDisplayPriceDiv'><span>BLOCKS</span></div>
+                        <div className='infoDisplayValueDiv'>
+                            <div className='infoDisplayValueDiv1'>
+                                <span>$ 1, 182.32</span>
+                            </div>
+                            <div className='infoDisplayValueDiv2'>
+                                <span>2.58</span><i class="fa-solid fa-angle-down"></i>
+                            </div>
+                            <div className='vh3'></div>
                         </div>
-                        <div className='infoDisplayValueDiv2'>
-                            <span>2.58</span><i class="fa-solid fa-angle-down"></i>
-                        </div>
-                        <div className='vh3'></div>
                     </div>
-                </div>
 
-                <div className='infoDisplayFirstDiv'>
-                    <i class="fa-solid fa-earth-oceania"></i><div className='infoDisplayPriceDiv'><span>MARKET CAP</span></div>
-                    <div className='infoDisplayValueDiv'>
-                        <div className='infoDisplayValueDiv1'>
-                            <span>$ 1, 182.32</span>
-                        </div>
-                        <div className='infoDisplayValueDiv2'>
-                            <span>2.58</span><i class="fa-solid fa-angle-down"></i>
-                        </div>
+                    <div className='infoDisplayFirstDiv'>
+                        <i class="fa-solid fa-earth-oceania"></i><div className='infoDisplayPriceDiv'><span>MARKET CAP</span></div>
+                        <div className='infoDisplayValueDiv'>
+                            <div className='infoDisplayValueDiv1'>
+                                <span>$ 1, 182.32</span>
+                            </div>
+                            <div className='infoDisplayValueDiv2'>
+                                <span>2.58</span><i class="fa-solid fa-angle-down"></i>
+                            </div>
 
+                        </div>
                     </div>
-                </div>
 
-                <div className='infoDisplayFirstDiv'>
-                    <i class="fa-solid fa-clock"></i><div className='infoDisplayPriceDiv'><span>TRANSACTIONS PER SEC</span></div>
-                    <div className='infoDisplayValueDiv'>
-                        <div className='infoDisplayValueDiv1'>
-                            <span>$ 1, 182.32</span>
-                        </div>
-                        <div className='infoDisplayValueDiv2'>
-                            <span>2.58</span><i class="fa-solid fa-angle-down"></i>
-                        </div>
+                    <div className='infoDisplayFirstDiv'>
+                        <i class="fa-solid fa-clock"></i><div className='infoDisplayPriceDiv'><span>TRANSACTIONS PER SEC</span></div>
+                        <div className='infoDisplayValueDiv'>
+                            <div className='infoDisplayValueDiv1'>
+                                <span>$ 1, 182.32</span>
+                            </div>
+                            <div className='infoDisplayValueDiv2'>
+                                <span>2.58</span><i class="fa-solid fa-angle-down"></i>
+                            </div>
 
+                        </div>
                     </div>
-                </div>
 
-                <div className='infoDisplayFirstDiv infoDisplayFirstDivMGP'>
-                    <i class="fa-solid fa-dollar-sign"></i><div className='infoDisplayPriceDiv'><span>MED. GAS PRICE</span></div>
-                    <div className='infoDisplayValueDiv'>
-                        <div className='infoDisplayValueDiv1'>
-                            <span>$ 1, 182.32</span>
-                        </div>
-                        <div className='infoDisplayValueDiv2'>
-                            <span>2.58</span><i class="fa-solid fa-angle-down"></i>
-                        </div>
+                    <div className='infoDisplayFirstDiv infoDisplayFirstDivMGP'>
+                        <i class="fa-solid fa-dollar-sign"></i><div className='infoDisplayPriceDiv'><span>MED. GAS PRICE</span></div>
+                        <div className='infoDisplayValueDiv'>
+                            <div className='infoDisplayValueDiv1'>
+                                <span>$ 1, 182.32</span>
+                            </div>
+                            <div className='infoDisplayValueDiv2'>
+                                <span>2.58</span><i class="fa-solid fa-angle-down"></i>
+                            </div>
 
+                        </div>
                     </div>
+
+
                 </div>
-
-
-            </div>
             </div>
             <section>
-                <div><p>Ronan</p></div>
+                <div className='footer'>
+                    <div className='footer1'>
+                        <div className='navDiv_logoDiv'><i className="fa-brands fa-ethereum"></i><span>ETHEXPLORER</span></div>
+                        <div><p><strong>ETHExplorer</strong> is a Blockchain platform for Ethereum.</p></div>
+                        <div></div>
+                    </div>
+
+                    <div className='footer2'>
+                        <div><span><strong>Blockchain</strong></span></div>
+                        <div>
+                            <ul>
+                                <li>Transactions</li>
+                                <li>Blocks</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='footer3'>
+                        <div><span><strong>About</strong></span></div>
+                        <div>
+                            <ul>
+                                <li>About Ethexplorer</li>
+                                <li>Terms of service</li>
+                                <li>Privacy Policy</li>
+                                <li>FAQS</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='footer4'>
+                        <div><span><strong>Languages</strong></span></div>
+                        <div>
+                            <ul>
+                                <li>English</li>
+                                <li>Arabic</li>
+                                <li>French</li>
+                                <li>German</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='lastSection'>
+                    <div className='lastSection1'>
+                        <p>Ethexplorer</p>
+                        <i class="fa-regular fa-registered"></i>
+                        <p>2022</p>
+                    </div>
+
+                    <div className='lastSection2'>
+                        <p><i class="fa-brands fa-facebook"></i></p>
+                        <p><i class="fa-brands fa-github"></i></p>
+                        <p><i class="fa-brands fa-linkedin"></i></p>
+                        <p><i class="fa-brands fa-instagram"></i></p>
+                    </div>
+                </div>
             </section>
         </div>
-        
+
 
     );
 }
--- src/components/Test.js
+++ src/components/Test.js
@@ -18,3 +18,63 @@ const Alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P
     const number = Math.floor(Math.random()*27);
     return Alphabet[number];
 })();
+
+
+<div className='footer'>
+<div className='footer1'>
+<div className='navDiv_logoDiv'><i className="fa-brands fa-ethereum"></i><span>ETHEXPLORER</span></div>
+    <div><p><strong>ETHExplorer</strong> is a Blockchain platform for Ethereum</p></div>
+    <div></div>
+</div>
+
+<div className='footer2'>
+    <div><span><strong>Blockchain</strong></span></div>
+    <div>
+        <ul>
+            <li>Transactions</li>
+            <li>Blocks</li>
+        </ul>
+    </div>
+</div>
+
+<div className='footer3'>
+    <div><span><strong>About</strong></span></div>
+    <div>
+        <ul>
+            <li>About Ethexplorer</li>
+            <li>Terms of service</li>
+            <li>Privacy Policy</li>
+            <li>FAQS</li>
+        </ul>
+    </div>
+</div>
+
+<div className='footer4'>
+    <div><span><strong>Languages</strong></span></div>
+    <div>
+        <ul>
+            <li>English</li>
+            <li>Arabic</li>
+            <li>French</li>
+            <li>German</li>
+        </ul>
+    </div>
+</div>
+
+</div>
+
+
+
+
+//<div className='footer1'>
+//                    ll
+//                    </div>
+//                    <div className='footer1'>
+//                    ll
+//                    </div>
+//                    <div className='footer1'>
+//                    ll
+//                    </div>
+//                    <div className='footer1'>
+//                    ll
+//                    </div>
